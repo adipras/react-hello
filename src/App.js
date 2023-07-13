dd61efeb4b9c331185dd934acf8154f787c29a41
adipras <tottiprasetyo@gmail.com>
2023-07-13 09:09:01 +0700

    EditEmployee

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
 import './index.css';
-import Employee from './components/Employee.js';
+import Employee from './components/Employee';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
 
 function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: 'Darboe',
       role: 'developer',
       img: 'https://images.pexels.com/photos/2071882/pexels-photo-2071882.jpeg?auto=compress&cs=tinysrgb&w=1600'
     },
     {
+      id: 2,
       name: 'Bove',
       role: 'QA',
       img: 'https://images.pexels.com/photos/208984/pexels-photo-208984.jpeg?auto=compress&cs=tinysrgb&w=1600'
     },
     {
+      id: 3,
       name: 'Matic',
       role: 'UI/UX',
       img: 'https://images.pexels.com/photos/982300/pexels-photo-982300.jpeg?auto=compress&cs=tinysrgb&w=1600'
     },
     {
+      id: 4,
       name: 'Ibanez',
       role: 'software enginer',
       img: 'https://images.pexels.com/photos/1573324/pexels-photo-1573324.jpeg?auto=compress&cs=tinysrgb&w=1600'
     },
   ]);
+  
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return {...employee, name: newName, role: newRole};
+      }
+
+      return employee;
+    });
+    
+    setEmployees(updatedEmployees);
+  }
+
+  function newEmployee(name, role, img) {
+     const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+     }
+     setEmployees([...employees, newEmployee])
+  }
 
   const showEmployees = true;
   return (
@@ -41,15 +69,26 @@ function App() {
            />
           <div className="flex flex-wrap justify-center">
             {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  updateEmployee={updateEmployee}
+                />
+              );
               return (
                 <Employee 
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id }
                   name={employee.name} 
                   role={employee.role} 
-                  img={employee.img} 
+                  img={employee.img}
+                  editEmployee={editEmployee}
                 />);
             })}
           </div>
+          <AddEmployee newEmployee={newEmployee} />
         </>
       ) : (
         <p>You cannot see the employees</p>
--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -2,9 +2,10 @@ import { useState } from 'react';
 // import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee(props) {
-  const [name, setName] = useState(props.name);
-  const [role, setRole] = useState(props.role);
+function AddEmployee(props) {
+  const [name, setName] = useState('');
+  const [role, setRole] = useState('');
+  const [img, setImg ] = useState('');
 
   const [show, setShow] = useState(false);
 
@@ -13,8 +14,8 @@ function EditEmployee(props) {
 
   return (
     <>
-      <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-        Update
+      <button onClick={handleShow} className="block mx-auto m-2 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+        + Add Employee
       </button>
 
       <Modal
@@ -24,13 +25,15 @@ function EditEmployee(props) {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Update Employee</Modal.Title>
+          <Modal.Title>Add Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <form onSubmit={(e) => {
-            handleClose();
             e.preventDefault();
-            props.updateEmployee(props.id, name,  role);
+            setName('');
+            setRole('');
+            setImg('');
+            props.newEmployee(name,  role, img);
           }}
           id='editmodal' className="w-full max-w-sm">
             <div className="md:flex md:items-center mb-6">
@@ -41,8 +44,9 @@ function EditEmployee(props) {
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="name"
+                  placeholder='ex. Abdur'
                   type="text"
                   value={name}
                   onChange={(e) => {
@@ -59,12 +63,32 @@ function EditEmployee(props) {
               </div>
               <div className="md:w-2/3">
                 <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   id="role"
+                  placeholder='ex. freelance SE'
                   type="text"
                   value={role}
                   onChange={(e) => {
                     setRole(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="img">
+                  Image URL
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="img"
+                  placeholder='ex. https://sakacode.id'
+                  type="text"
+                  value={img}
+                  onChange={(e) => {
+                    setImg(e.target.value);
                   }}
                 />
               </div>
@@ -75,7 +99,7 @@ function EditEmployee(props) {
           <button className='bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded' onClick={handleClose}>
             Close
           </button>
-          <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded' form="editmodal">
+          <button className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded' onClick={handleClose} form="editmodal">
             Save
           </button>
         </Modal.Footer>
@@ -84,4 +108,4 @@ function EditEmployee(props) {
   );
 }
 
-export default EditEmployee;
+export default AddEmployee;
